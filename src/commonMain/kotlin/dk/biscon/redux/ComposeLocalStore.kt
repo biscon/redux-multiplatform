5e880fb97ebc9a4f59f8b636bd34055744c1af0a
bison <stinus.petersen@ufst.dk>
2023-03-09 13:50:59 +0100

    android specific implementations

---
+++ src/commonMain/kotlin/dk/biscon/redux/ComposeLocalStore.kt
@@ -0,0 +1,17 @@
+package dk.biscon.redux
+
+import androidx.compose.runtime.DisallowComposableCalls
+import androidx.compose.runtime.State
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+interface ComposeLocalStore<Value, Action> {
+    val state: State<Value>
+    fun send(action: Action)
+}
+
+expect inline fun <LocalValue, LocalAction, GlobalValue, reified GlobalAction, GlobalEnvironment> rememberLocalStore(
+    globalStore: GlobalStore<GlobalValue, GlobalAction, GlobalEnvironment>,
+    dispatcher: CoroutineDispatcher = Dispatchers.Default,
+    crossinline getLocalCopy: @DisallowComposableCalls (GlobalValue) -> LocalValue
+): ComposeLocalStore<LocalValue, LocalAction>
